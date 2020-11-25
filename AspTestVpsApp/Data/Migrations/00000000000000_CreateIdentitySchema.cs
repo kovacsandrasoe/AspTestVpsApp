495b4d9584fc6037d8f664cf8eeb3f538d14d720
Kovács András <kovi91@gmail.com>
2020-11-25 11:36:25 +0100

    migration modded

--- AspTestVpsApp/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ AspTestVpsApp/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -15,7 +15,7 @@ namespace AspTestVpsApp.Data.Migrations
                     Id = table.Column<string>(nullable: false),
                     Name = table.Column<string>(maxLength: 256, nullable: true),
                     NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
-                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                    ConcurrencyStamp = table.Column<string>(maxLength: 256, nullable: true)
                 },
                 constraints: table =>
                 {
